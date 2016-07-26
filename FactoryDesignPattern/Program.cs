ff1ca7245e7f860b8f9e45f6bed9840adca1e071
adeeb <adeeb@DESKTOP-QKITK8D>
2016-07-26 16:40:18 +0100

    Factory design pattern added. Note: With this pattern, we develop an abstraction that isolates the logic for determining which type of class to create.

---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                var position = Factory.Get(i);
+                Console.WriteLine("Where id = {0}, position = {1} ", i, position.Title);
+            }
+        }
+    }
+
+    // 1. Create abstract base class
+    abstract class Position
+    {
+        public abstract string Title { get; }
+    }
+
+    // 2. Get classes to inherit from abstract base class
+    class Manager : Position
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Manager";
+            }
+        }
+    }
+
+    class Clerk : Position
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Clerk";
+            }
+        }
+    }
+
+    class Programmer : Position
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Programmer";
+            }
+        }
+    }
+
+    static class Factory
+    {
+        /// <summary>
+        /// Decides which class to instantiate.
+        /// </summary>
+        public static Position Get(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return new Manager(); // example of Polymorphism
+                case 1:
+                case 2:
+                    return new Clerk(); // example of Polymorphism
+                case 3:
+                default:
+                    return new Programmer(); // example of Polymorphism
+            }
+        }
+    }
+}
